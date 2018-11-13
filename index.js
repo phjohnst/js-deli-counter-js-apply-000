0d4899dfa691c2810fa4310eb8afe78eb9387de8
Patrick Johnston <patrick.hardy.johnston@gmail.com>
2018-11-13 16:15:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-var 
+let katzDeliLine = [];
+
+function takeANumber(deliLine, name) {
+  deliLine.push(name);
+  return `Welcome, ${name}. You are number ${deliLine.length + 1} in line.`;
+}
+
+function nowServing(deliLine) {
+  let servingNow = deliLine[0];
+  
+  if (deliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  } else {
+    deliLine.shift();
+    return servingNow;
+  }
+}
+
+function currentLine(deliLine) {
+  let deliLineString = [];
+  
+  if (deliLine.length === 0) {
+    return "The line is currently empty.";
+  } else {
+    for (let i in deliLine.length) {
+      deliLineString = [...deliLineString, (i + 1) + `. ${deliLine[i]}`];
+    } 
+    if (deliLine.length === 1) {
+      return `The line is currently: ${deliLineString[0]}`;
+    } else {
+      for (let j in deliLine.length) {
+        return `The line is currently: ${deliLineString[j]}, ${deliLineString[j+1]}.`;
+      }
+    }
+  }
+}
