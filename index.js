f33d9f039d937262029527474f8ef3cab455c56f
Patrick Johnston <patrick.hardy.johnston@gmail.com>
2018-11-13 16:15:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let katzDeliLine = [];
 
 function takeANumber(deliLine, name) {
   deliLine.push(name);
-  return `Welcome, ${name}. You are number ${deliLine.length + 1} in line.`;
+  return `Welcome, ${name}. You are number ${deliLine.length} in line.`;
 }
 
 function nowServing(deliLine) {
